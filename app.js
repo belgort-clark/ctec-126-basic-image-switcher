29e9b579b5a4c641de2c0f51cfb33afc98a0f556
belgort-clark <belgort@clark.edu>
2021-04-10 10:38:53 -0700

    Updated for Spring 2021

--- app.js
+++ app.js
@@ -1,32 +1,46 @@
 // app.js
 
+// Get handle to the img tag with the id of #image1 
 const image = document.querySelector('#image1')
-const pics = ['100-400x400.jpg', '101-400x400.jpg', '401-400x400.jpg', '501-400x400.jpg', '830-400x400.jpg']
+// Declare an array that holds the file paths to 5 image
+const pics = ['images/100-400x400.jpg', 'images/101-400x400.jpg', 'images/401-400x400.jpg', 'images/501-400x400.jpg', 'images/830-400x400.jpg']
 
+// Declare two variables. Note that they are not set to any value. By default JavaScript will set them to undefined
 let current
 let timer
 
+// Function to switch out the image
 function startSwitcher() {
-    let position = Math.floor(Math.random() * 5)
-    if (current == position) {
-        position = Math.floor(Math.random() * 5)
+    // Generate a random number
+    // Reference: https://www.w3schools.com/js/js_math.asp
+    let index = Math.floor(Math.random() * (pics.length - 1))
+    // Check to make sure the index is not the current index (the current image)
+    if (current == index) {
+        index = Math.floor(Math.random() * (pics.length - 1))
     }
-    current = position
-    image.setAttribute('src', 'images/' + pics[position])
-
+    // Save the current index in a variable
+    current = index
+    // Change the img's src attribute
+    image.setAttribute('src', pics[index])
 }
 
+// Function to stop the switcher
 function stopSwitcher() {
+    // Clear the interval that was created
     clearInterval(timer)
     console.log('The animation has been stopped')
 }
 
+// Function to start the image switcher
 function start() {
-    timer = setInterval(startSwitcher, 1000)
+    // Create an interval that executes very 3 seconds and calls the startSwitcher function
+    timer = setInterval(startSwitcher, 3000)
     console.log('The animation has been started')
 }
 
+// Bind event listeners to the image for mouseover and mouseout events
 image.addEventListener('mouseover', stopSwitcher)
 image.addEventListener('mouseout', start)
 
+// Start the entire process
 start()
